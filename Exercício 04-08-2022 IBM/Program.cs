b0baea0ae34e9ed84f2b1442acd94f9cba38cb09
Mateus Melo Oliveira <mateus.melo@ibm.com>
2022-08-04 18:09:59 -0300

    Upando lição para o github

---
+++ Exercício 04-08-2022 IBM/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization; 
+
+namespace Exercício_04_08_2022_IBM {
+    class Program {
+        static void Main(string[] args) {
+
+            double raio;
+            double area;
+            Console.WriteLine("Digite o raio, no formato 0.0");
+
+            raio = double.Parse(Console.ReadLine());
+
+            area = Math.Pow(raio, 2) * 3.14159;
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
